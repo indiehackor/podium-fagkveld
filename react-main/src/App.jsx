b37270efe8480871e7676e3ff9338b981ee868be
Vegard Flatoy <vhflat@gmail.com>
2021-12-01 15:33:13 +0100

    fix react main content podlet

---
+++ react-main/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        background: "#3a86ff",
+        padding: 24,
+        border: "1px dashed red",
+        color: "white",
+        fontFamily: "'Source Code Pro', monospace",
+      }}
+    >
+      <h1>Hello World From React-Rollup</h1>
+    </div>
+  );
+}
+export default App;
