b9cf002ea4bebe0600bdaa8a3adbf265e30d7449
Vegard Flatoy <vhflat@gmail.com>
2021-12-02 10:54:27 +0100

    run layout server last so I don't have to switch tabs

--- run-all.sh
+++ run-all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ttab -d ./layout-server npm run start -t "Layout Server";
 ttab -d ./html-header npm run start -t "Header Podlet";
 ttab -d ./html-footer npm run start -t "Footer Podlet";
 ttab -d ./react-main npm run start -t "Main Content Podlet";
 ttab -d ./vue-sidebar npm run start -t "Sidebar Podlet";
+ttab -d ./layout-server npm run start -t "Layout Server";
+
